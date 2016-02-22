f2dc06b25a363ee2f189338e05dfed78ad78da64
aabalalonso <aabalalonso@atanasoff16.danielcastelao.org>
2016-02-22 09:09:04 +0100

    commit 2 , segunda variable añadida

---
+++ ejerciciomerge/src/ejerciciomerge/Ejerciciomerge.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciomerge;
+
+/**
+ *
+ * @author aabalalonso
+ */
+public class Ejerciciomerge {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int numero1=0;
+        int numero2=3;
+    }
+    
+}
